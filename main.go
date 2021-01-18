9b2064d4d3397535bde5ee926340775146dcc2ff
yykhomenko <yykhomenko@gmail.com>
2021-01-18 22:05:14 +0200

    ref

--- main.go
+++ main.go
@@ -20,22 +20,7 @@ func main() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID |
-			syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
-		UidMappings: []syscall.SysProcIDMap{
-			{
-				ContainerID: 1234,
-				HostID:      0,
-				Size:        1,
-			},
-		},
-		GidMappings: []syscall.SysProcIDMap{
-			{
-				ContainerID: 1234,
-				HostID:      0,
-				Size:        1,
-			},
-		},
+		Cloneflags: syscall.CLONE_NEWUTS,
 	}
 
 	must(cmd.Run())
