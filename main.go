91b66af915b39dd3991143994232206c3b96760d
yykhomenko <yykhomenko@gmail.com>
2021-01-18 22:35:40 +0200

    added child fork

--- main.go
+++ main.go
@@ -11,23 +11,34 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("unknown command: " + os.Args[1])
 	}
+}
 
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+func run() {
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
 	}
 
 	must(cmd.Run())
 }
 
-func run() {
-	fmt.Printf("running %v\n", os.Args[2:])
+func child() {
+	fmt.Printf("running %v as PID %d\n", os.Args[2:], os.Getpid())
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	must(cmd.Run())
 }
 
 func must(err error) {
