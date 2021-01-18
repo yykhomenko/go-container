008c129c93612fce3cb234f763db1478aed7ceba
yykhomenko <yykhomenko@gmail.com>
2021-01-18 16:28:11 +0200

    added run

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	println("hello world")
+	switch os.Args[1] {
+	case "run":
+		run()
+	default:
+		panic("unknown command: " + os.Args[1])
+	}
+}
+
+func run() {
+	fmt.Printf("running %v\n", os.Args[2:])
 }
