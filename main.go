40104ee2a0e2160e060d754333027e07f6d3a151
yykhomenko <yykhomenko@gmail.com>
2021-01-18 22:01:58 +0200

    fix security bug

--- main.go
+++ main.go
@@ -20,7 +20,22 @@ func main() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 1234,
+				HostID:      0,
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 1234,
+				HostID:      0,
+				Size:        1,
+			},
+		},
 	}
 
 	must(cmd.Run())
