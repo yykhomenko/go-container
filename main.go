f84782d85fac93d32d02f0e7f5feee1135b54043
yykhomenko <yykhomenko@gmail.com>
2021-01-19 00:10:32 +0200

    added namespaces

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"runtime"
 	"syscall"
 )
 
@@ -24,24 +25,26 @@ func run() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
 	}
-
 	must(cmd.Run())
 }
 
 func child() {
 	fmt.Printf("running %v as PID %d\n", os.Args[2:], os.Getpid())
-
+	runtime.LockOSThread()
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
+	must(syscall.Sethostname([]byte("my-container")))
 	must(syscall.Chroot("/home/rootfs"))
 	must(os.Chdir("/"))
 	must(syscall.Mount("proc", "proc", "proc", 0, ""))
 	must(cmd.Run())
+	must(syscall.Unmount("/proc", 0))
 }
 
 func must(err error) {
